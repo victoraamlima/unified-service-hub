52bce24d71ce726666a4bc5641b6b7851020c3f6
Victor Meireles <victoraamlima@gmail.com>
2025-02-06 12:56:40 -0300

    Remove unused TicketHome component

--- src/components/Ticket.tsx
+++
@@ -1,9 +0,0 @@
-export default function TicketHome() {
-  return (
-    <main className="flex-center flex-col h-screen">
-      <h2>Ticket</h2>
-      <button>Novo</button>
-      <button>Acompanhar</button>
-    </main>
-  );
-}
