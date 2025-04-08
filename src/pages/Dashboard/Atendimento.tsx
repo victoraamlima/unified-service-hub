a64b000d30acbeda6594cf035a9b79059e5e5f64
Victor Meireles <victoraamlima@gmail.com>
2025-04-08 11:34:46 -0300

    Enhance Atendimento layout: add Header and wrap TicketHome in BgMainPanel for improved structure

--- src/pages/Dashboard/Atendimento.tsx
+++ src/pages/Dashboard/Atendimento.tsx
@@ -1,5 +1,15 @@
+import BgMainPanel from "@/components/BgMainPanel";
 import TicketHome from "../../components/Tickets/TicketHome";
+import Header from "@/components/Dashboard/Header";
 
 export default function Atendimento() {
-  return <TicketHome />;
+  return (
+    <main className="flex min-h-screen">
+      <Header />
+
+      <BgMainPanel>
+        <TicketHome />
+      </BgMainPanel>
+    </main>
+  );
 }
