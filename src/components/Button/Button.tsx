0221c4911cfc05d8a69f0d7b0292c79ce1a75631
Victor Meireles <victoraamlima@gmail.com>
2025-04-08 11:34:00 -0300

    Add style prop to Button component for flexible styling options

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,31 @@
-import { useAppDesignContext } from "@/contexts/design";
-
 interface ButtonProps {
   onClick: () => void;
   name: string;
   type: "button" | "submit" | "reset";
   width: string;
+  style: string;
 }
 
-export default function Button({ onClick, name, type, width }: ButtonProps) {
+export default function Button({
+  onClick,
+  name,
+  type,
+  width,
+  style,
+}: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className={`self-center ${width} uppercase h-12 rounded-2xl text-white font-bold text-sm bg-blue5 hover:bg-blue4 text-white1 easy-in-out duration-100`}
+      className={`${width} uppercase h-12 rounded-2xl text-white font-bold text-sm transition-all px-6 ${
+        style === "style-1"
+          ? `bg-blue5 hover:bg-blue4 text-white1`
+          : style === "style-2"
+          ? `bg-white1 hover:bg-blue4 hover:text-white1 text-blue5`
+          : style === "style-3"
+          ? `bg-blue5 hover:bg-blue4 text-white1`
+          : `bg-blue5 hover:bg-blue4 text-white1`
+      }
+        `}
       type={type}
     >
       {name}
